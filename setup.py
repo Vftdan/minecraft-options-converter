202f862ab3df66fbbf48a85b4297902aa6610e53
Vftdan <vftdan@gmail.com>
2023-01-21 18:04:31 +0300

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='minecraft-options-converter',
-    version='0.0.1',
+    version='0.0.2',
     description='Minecraft options.txt keycode converter',
     author='Vftdan',
     packages=find_packages(),
